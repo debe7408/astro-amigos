8857ca655e255be9b10ace874bf4b7aeb30715d2
dBaliukynas <domas.baliukynas@gmail.com>
2023-10-07 14:44:06 +0300

    feat: changed main page

--- frontend/amigos-frontend/src/app/layout.tsx
+++ frontend/amigos-frontend/src/app/layout.tsx
@@ -58,53 +58,11 @@ export default function RootLayout({
               </Typography>
             </Toolbar>
           </AppBar>
-          <Drawer
-            sx={{
-              width: DRAWER_WIDTH,
-              flexShrink: 0,
-              "& .MuiDrawer-paper": {
-                width: DRAWER_WIDTH,
-                boxSizing: "border-box",
-                top: ["48px", "56px", "64px"],
-                height: "auto",
-                bottom: 0,
-              },
-            }}
-            variant="permanent"
-            anchor="left"
-          >
-            <Divider />
-            <List>
-              {LINKS.map(({ text, href, icon: Icon }) => (
-                <ListItem key={href} disablePadding>
-                  <ListItemButton component={Link} href={href}>
-                    <ListItemIcon>
-                      <Icon />
-                    </ListItemIcon>
-                    <ListItemText primary={text} />
-                  </ListItemButton>
-                </ListItem>
-              ))}
-            </List>
-            <Divider sx={{ mt: "auto" }} />
-            <List>
-              {PLACEHOLDER_LINKS.map(({ text, icon: Icon }) => (
-                <ListItem key={text} disablePadding>
-                  <ListItemButton>
-                    <ListItemIcon>
-                      <Icon />
-                    </ListItemIcon>
-                    <ListItemText primary={text} />
-                  </ListItemButton>
-                </ListItem>
-              ))}
-            </List>
-          </Drawer>
           <Box
             component="main"
             sx={{
               flexGrow: 1,
-              bgcolor: "background.default",
+      
               ml: `${DRAWER_WIDTH}px`,
               mt: ["48px", "56px", "64px"],
               p: 3,
